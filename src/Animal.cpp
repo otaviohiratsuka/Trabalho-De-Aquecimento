f2afd74a7e6fd5d63d4adfab52878a2d6fe1d1b1
hiratsukaotavio <otaviohiratsukac@gmail.com>
2025-04-08 11:39:11 -0300

    I still see your shadows in my room

--- src/Animal.cpp
+++ src/Animal.cpp
@@ -19,11 +19,50 @@ void Animal :: chegouAgua (Floresta& floresta){
     int dx[] = {-1, 1, 0, 0};
     int dy[] = {0, 0, -1, 1};
 
-    for (int d = 0; d < 4; ++d) {
+    for (int d = 0; d < 4; d++){
         int nx = x + dx[d];
         int ny = y + dy[d];
         if (floresta.dentroDosLimites(nx, ny)) {
             floresta.setEstado(nx, ny, 1);
         }
+    }
+}
+
+void Animal ::mover(Floresta& floresta){
+    int melhorX = x, melhorY= y, melhorValor = floresta.getEstado(x,y);
+    
+    int dx[] = {-1, 1, 0, 0};
+    int dy[] = {0, 0, -1, 1};
+
+    for (int d = 0; d < 4; d++){
+        int nx =  x + dx[d], ny = y + dy[d];
+        if (floresta.dentroDosLimites(nx,ny)){
+            int estado = floresta.getEstado(nx,ny);
+            if (estado == 4){
+                melhorX = nx; melhorY = ny; melhorValor = 4;
+                break;
+            }
+            else if ((estado == 0 || estado == 1) && melhorValor != 4){
+                melhorX = nx; melhorY = ny; melhorValor = estado;
+            }  
+        }
+    }   
+    if (melhorX != x || melhorY != y){
+        x = melhorX; y = melhorY;
+        ciclosPreso = 0;
+        passos++;
+
+        if (melhorValor == 4){
+            chegouAgua(floresta);
+        }
+    }
+    else{
+        ciclosPreso++;
+    }
+}
+
+void Animal :: checkQueimou(Floresta& floresta){
+    if(floresta.getEstado(x,y) == 2 ){
+        vivo = false; // animal morto
     }
 }
